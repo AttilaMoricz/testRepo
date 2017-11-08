4af590ecaadec173428eafa819bc669d0ad75db7
AttilaMoricz <qmtzz0@gamil.com>
2017-11-08 22:51:16 +0100

    My first branch

--- moricz/src/main/java/anotherPackage/PrintTheMessage.java
+++ moricz/src/main/java/anotherPackage/PrintTheMessage.java
@@ -3,7 +3,7 @@ package anotherPackage;
 public class PrintTheMessage {
 
     public static void printThis(String message) {
-        System.out.println(message);
+        System.out.println(message + " Test branch");
     }
 
 }
