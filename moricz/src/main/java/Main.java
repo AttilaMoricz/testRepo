263dd99d81e91e0d806d5d51ec55c96688324cb1
AttilaMoricz <qmtzz0@gamil.com>
2017-11-08 22:39:20 +0100

    This is just a test commit message3333

--- moricz/src/main/java/Main.java
+++ moricz/src/main/java/Main.java
@@ -1,8 +1,10 @@
+import static anotherPackage.DoNotPrint.notPrint;
 import static anotherPackage.PrintTheMessage.printThis;
 
 public class Main {
 
     public static void main(String[] args) {
         printThis("This is a test text");
+        notPrint("This is another text");
     }
 }
---
+++ moricz/src/main/java/anotherPackage/DoNotPrint.java
@@ -0,0 +1,7 @@
+package anotherPackage;
+
+public class DoNotPrint {
+    public static void notPrint(String text) {
+        return;
+    }
+}
